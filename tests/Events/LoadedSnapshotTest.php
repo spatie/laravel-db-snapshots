e9c95daa7305033a7268677b9319471793f737a9
freek <freek@spatie.be>
2017-03-21 11:19:48 +0100

    commit

---
+++ tests/Events/LoadedSnapshotTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Spatie\DbSnapshots\Commands\Test;
+
+use Spatie\DbSnapshots\Test\TestCase;
+use Event;
+use Artisan;
+use Spatie\DbSnapshots\Events\LoadedSnapshot;
+
+class LoadedSnapshotTest extends TestCase
+{
+    /** @test */
+    public function after_a_snapshot_has_been_loaded_the_loaded_snapshot_event_will_be_fired()
+    {
+        Event::fake();
+
+        Artisan::call('snapshots:load', ['name' => 'snapshot2']);
+
+        Event::assertDispatched(LoadedSnapshot::class, function(LoadedSnapshot $event) {
+            return $event->snapshot->fileName === 'snapshot2.sql';
+        });
+    }
+}
---
+++ tests/Events/LoadingSnapshotTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Spatie\DbSnapshots\Commands\Test;
+
+use Spatie\DbSnapshots\Test\TestCase;
+use Event;
+use Artisan;
+use Spatie\DbSnapshots\Events\LoadingSnapshot;
+
+class LoadingSnapshotTest extends TestCase
+{
+    /** @test */
+    public function loading_a_snapshot_fires_the_loading_snapshot_event()
+    {
+        Event::fake();
+
+        Artisan::call('snapshots:load', ['name' => 'snapshot2']);
+
+        Event::assertDispatched(LoadingSnapshot::class, function(LoadingSnapshot $event) {
+            return $event->snapshot->fileName === 'snapshot2.sql';
+        });
+    }
+}
