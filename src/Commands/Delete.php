927530944f94b2bbe69c148c177c82aa5902750a
michael <michael@estsurinter.net>
2018-04-18 11:51:15 +0200

    Prevent Exception when trying to delete snapshots when there are none
    If one calls the `snapshot:delete` command without providing a snapshot name and if there is no available snapshot, a \LogicException will be thrown when trying to build a list of choices.
    
    This guards against the exception in a similar way to what is done within the `snapshot:list` and `snapshot:load` commands.

--- src/Commands/Delete.php
+++ src/Commands/Delete.php
@@ -16,6 +16,14 @@ class Delete extends Command
 
     public function handle()
     {
+        $snapShots = app(SnapshotRepository::class)->getAll();
+
+        if (app(SnapshotRepository::class)->getAll()->isEmpty()) {
+            $this->warn('No snapshots found. Run `snapshot:create` to create snapshots.');
+
+            return;
+        }
+
         $name = $this->argument('name') ?: $this->askForSnapshotName();
 
         $snapshot = app(SnapshotRepository::class)->findByName($name);
