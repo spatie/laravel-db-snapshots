6422a18287a14d5860ea7df0fadcc651425a936d
Josh Matheney <jamatheney@gmail.com>
2019-10-31 10:51:42 -0500

    Remove unused Use statement

--- tests/Commands/CleanupTest.php
+++ tests/Commands/CleanupTest.php
@@ -2,29 +2,27 @@
 
 namespace Spatie\DbSnapshots\Commands\Test;
 
-use Mockery as m;
 use Spatie\DbSnapshots\Test\TestCase;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Storage;
 
 class CleanupTest extends TestCase
 {
-    /** @test */
-    public function it_can_delete_old_snapshots_keeping_the_desired_number_of_snapshots()
-    {
-        // Add sleep to make sure files do not have the same modified time.
-        // They may not sort properly if all have the same timestamp.
-        $this->clearDisk();
-       
-        $this->disk->put("snapshot1.sql", 'new content');
-        
-        sleep(1);
-        
-        $this->disk->put("snapshot2.sql", 'new content');
-        
-        Artisan::call('snapshot:cleanup', ['--keep' => 1]);
+	/** @test */
+	public function it_can_delete_old_snapshots_keeping_the_desired_number_of_snapshots()
+	{
+		// Add sleep to make sure files do not have the same modified time.
+		// They may not sort properly if all have the same timestamp.
+		$this->clearDisk();
 
-        $this->disk->assertMissing('snapshot1.sql');
-        $this->disk->assertExists('snapshot2.sql');
-    }
+		$this->disk->put('snapshot1.sql', 'new content');
+
+		sleep(1);
+
+		$this->disk->put('snapshot2.sql', 'new content');
+
+		Artisan::call('snapshot:cleanup', ['--keep' => 1]);
+
+		$this->disk->assertMissing('snapshot1.sql');
+		$this->disk->assertExists('snapshot2.sql');
+	}
 }
