57e44b4488650ce519be58354c43856517b1b3b0
Eli Strauss-Reis <64713921+eli-s-r@users.noreply.github.com>
2022-06-17 16:38:12 -0700

    use null coalescing operator

--- src/SnapshotRepository.php
+++ src/SnapshotRepository.php
@@ -19,10 +19,7 @@ class SnapshotRepository
             ->filter(function (string $fileName) {
                 $pathinfo = pathinfo($fileName);
 
-                if (
-                    array_key_exists('extension', $pathinfo)
-                    && $pathinfo['extension'] === 'gz'
-                ) {
+                if (($pathinfo['extension'] ?? null) === 'gz') {
                     $fileName = $pathinfo['filename'];
                 }
 
