90b1bedc6f13d83e73978a985babfbe0c13c405e
Alex Manase <malexandru699@gmail.com>
2022-11-05 18:08:32 +0200

    refactor `DbDumperFactoryTest`

--- tests/DbDumperFactoryTest.php
+++ tests/DbDumperFactoryTest.php
@@ -8,147 +8,119 @@ use Spatie\DbDumper\Databases\Sqlite;
 use Spatie\DbSnapshots\DbDumperFactory;
 use Spatie\DbSnapshots\Exceptions\CannotCreateDbDumper;
 
-class DbDumperFactoryTest extends TestCase
+function getDumpCommand(string $connectionName): string
 {
-    public function setUp(): void
-    {
-        parent::setUp();
+    $dumpFile = '';
+    $credentialsFile = '';
 
-        $this->app['config']->set('database.default', 'mysql');
-
-        $dbConfig = [
-            'driver' => 'mysql',
-            'host' => 'localhost',
-            'username' => 'root',
-            'password' => 'myPassword',
-            'database' => 'myDb',
-            'dump' => ['add_extra_option' => '--extra-option=value'],
-        ];
-
-        $this->app['config']->set('database.connections.mysql', $dbConfig);
-    }
-
-    /** @test */
-    public function it_can_create_instances_of_mysql_and_pgsql()
-    {
-        $this->assertInstanceOf(MySql::class, DbDumperFactory::createForConnection('mysql'));
-        $this->assertInstanceOf(PostgreSql::class, DbDumperFactory::createForConnection('pgsql'));
-    }
-
-    /** @test */
-    public function it_can_create_sqlite_instance()
-    {
-        $this->app['config']->set('database.connections.sqlite', [
-            'driver' => 'sqlite',
-            'database' => 'database.sqlite',
-            // host, username and password are not required for the sqlite driver
-        ]);
-
-        $this->assertInstanceOf(Sqlite::class, DbDumperFactory::createForConnection('sqlite'));
-    }
-
-    /** @test */
-    public function it_will_use_the_read_db_when_one_is_defined()
-    {
-        $dbConfig = [
-            'driver' => 'mysql',
-            'read' => [
-                'host' => 'localhost-read',
-            ],
-            'write' => [
-                'host' => 'localhost-write',
-            ],
-            'username' => 'root',
-            'password' => 'myPassword',
-            'database' => 'myDb',
-            'dump' => ['add_extra_option' => '--extra-option=value'],
-        ];
-
-        $this->app['config']->set('database.connections.mysql', $dbConfig);
-
-        $dumper = DbDumperFactory::createForConnection('mysql');
-
-        $this->assertEquals('localhost-read', $dumper->getHost());
-    }
-
-    /** @test */
-    public function it_will_use_connect_via_database_when_one_is_defined()
-    {
-        $dbConfig = [
-            'driver' => 'pgsql',
-            'connect_via_database' => 'connection_pool',
-            'username' => 'root',
-            'password' => 'myPassword',
-            'database' => 'myDb',
-            'dump' => ['add_extra_option' => '--extra-option=value'],
-        ];
-
-        $this->app['config']->set('database.connections.pgsql', $dbConfig);
-
-        $dumper = DbDumperFactory::createForConnection('pgsql');
-
-        $this->assertEquals('connection_pool', $dumper->getDbName());
-    }
-
-    /** @test */
-    public function it_will_throw_an_exception_when_creating_an_unknown_type_of_dumper()
-    {
-        $this->expectException(CannotCreateDbDumper::class);
-
-        DbDumperFactory::createForConnection('unknown type');
-    }
-
-    /** @test */
-    public function it_will_throw_an_exception_when_no_disks_are_set_up()
-    {
-        config()->set('filesystem.disks', null);
-
-        $this->expectException(CannotCreateDbDumper::class);
-
-        DbDumperFactory::createForConnection('unknown type');
-    }
-
-    /** @test */
-    public function it_can_add_named_options_to_the_dump_command()
-    {
-        $dumpConfig = ['use_single_transaction'];
-
-        $this->app['config']->set('database.connections.mysql.dump', $dumpConfig);
-
-        $this->assertStringContainsString('--single-transaction', $this->getDumpCommand('mysql'));
-    }
-
-    /** @test */
-    public function it_can_add_named_options_with_an_array_value_to_the_dump_command()
-    {
-        $dumpConfig = ['include_tables' => ['table1', 'table2']];
-
-        $this->app['config']->set('database.connections.mysql.dump', $dumpConfig);
-
-        $this->assertStringContainsString(implode(' ', $dumpConfig['include_tables']), $this->getDumpCommand('mysql'));
-    }
-
-    /** @test */
-    public function it_can_add_arbritrary_options_to_the_dump_command()
-    {
-        $dumpConfig = ['add_extra_option' => '--extra-option=value'];
-
-        $this->app['config']->set('database.connections.mysql.dump', $dumpConfig);
-
-        $this->assertStringContainsString($dumpConfig['add_extra_option'], $this->getDumpCommand('mysql'));
-    }
-
-    /** @test */
-    public function it_adds_the_inserts_option_to_the_pgsql_dump_command()
-    {
-        $this->assertStringContainsString('--inserts', $this->getDumpCommand('pgsql'));
-    }
-
-    protected function getDumpCommand(string $connectionName): string
-    {
-        $dumpFile = '';
-        $credentialsFile = '';
-
-        return DbDumperFactory::createForConnection($connectionName)->getDumpCommand($dumpFile, $credentialsFile);
-    }
+    return DbDumperFactory::createForConnection($connectionName)->getDumpCommand($dumpFile, $credentialsFile);
 }
+
+beforeEach(function () {
+    $this->app['config']->set('database.default', 'mysql');
+
+    $dbConfig = [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'username' => 'root',
+        'password' => 'myPassword',
+        'database' => 'myDb',
+        'dump' => ['add_extra_option' => '--extra-option=value'],
+    ];
+
+    $this->app['config']->set('database.connections.mysql', $dbConfig);
+});
+
+it('can create instances of MySQL and pgSQL')
+    ->expect(fn () => DbDumperFactory::createForConnection('mysql'))
+    ->toBeInstanceOf(MySql::class)
+    ->and(fn () => DbDumperFactory::createForConnection('pgsql'))
+    ->toBeInstanceOf(PostgreSql::class);
+
+it('can create sqlite instance', function () {
+    $this->app['config']->set('database.connections.sqlite', [
+        'driver' => 'sqlite',
+        'database' => 'database.sqlite',
+        // host, username and password are not required for the sqlite driver
+    ]);
+
+    expect(DbDumperFactory::createForConnection('sqlite'))
+        ->toBeInstanceOf(Sqlite::class);
+});
+
+it('will use the read db when one is defined', function () {
+    $dbConfig = [
+        'driver' => 'mysql',
+        'read' => [
+            'host' => 'localhost-read',
+        ],
+        'write' => [
+            'host' => 'localhost-write',
+        ],
+        'username' => 'root',
+        'password' => 'myPassword',
+        'database' => 'myDb',
+        'dump' => ['add_extra_option' => '--extra-option=value'],
+    ];
+
+    $this->app['config']->set('database.connections.mysql', $dbConfig);
+
+    $dumper = DbDumperFactory::createForConnection('mysql');
+
+    expect($dumper->getHost())->toEqual('localhost-read');
+});
+
+it('will use connect via database when one is defined', function () {
+    $dbConfig = [
+        'driver' => 'pgsql',
+        'connect_via_database' => 'connection_pool',
+        'username' => 'root',
+        'password' => 'myPassword',
+        'database' => 'myDb',
+        'dump' => ['add_extra_option' => '--extra-option=value'],
+    ];
+
+    $this->app['config']->set('database.connections.pgsql', $dbConfig);
+
+    $dumper = DbDumperFactory::createForConnection('pgsql');
+
+    expect($dumper->getDbName())->toEqual('connection_pool');
+});
+
+it('will throw an exception when creating an unknown type of dumper', function () {
+    DbDumperFactory::createForConnection('unknown type');
+})->throws(CannotCreateDbDumper::class);
+
+it('will throw an exception when no disks are set up', function () {
+    config()->set('filesystem.disks', null);
+
+    DbDumperFactory::createForConnection('unknown type');
+})->throws(CannotCreateDbDumper::class);
+
+it('can add named options to the dump command', function () {
+    $dumpConfig = ['use_single_transaction'];
+
+    $this->app['config']->set('database.connections.mysql.dump', $dumpConfig);
+
+    expect(getDumpCommand('mysql'))->toContain('--single-transaction');
+});
+
+it('can add named options with an array value to the dump command', function () {
+    $dumpConfig = ['include_tables' => ['table1', 'table2']];
+
+    $this->app['config']->set('database.connections.mysql.dump', $dumpConfig);
+
+    expect(getDumpCommand('mysql'))->toContain(implode(' ', $dumpConfig['include_tables']));
+});
+
+it('can add arbitrary options to the dump command', function () {
+    $dumpConfig = ['add_extra_option' => '--extra-option=value'];
+
+    $this->app['config']->set('database.connections.mysql.dump', $dumpConfig);
+
+    expect(getDumpCommand('mysql'))->toContain($dumpConfig['add_extra_option']);
+});
+
+it('adds the inserts option to the pgSQL dump command')
+    ->expect(fn () => getDumpCommand('pgsql'))
+    ->toContain('--inserts');
