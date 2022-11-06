a81554b7f3e4cae40abc0c341b723104011f2a81
Alex Manase <malexandru699@gmail.com>
2022-11-06 16:59:49 +0200

    refactor `LoadTest`

--- tests/Commands/LoadTest.php
+++ tests/Commands/LoadTest.php
@@ -1,169 +1,145 @@
 <?php
 
-namespace Spatie\DbSnapshots\Commands\Test;
-
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Mockery as m;
-use Spatie\DbSnapshots\Test\TestCase;
 
-class LoadTest extends TestCase
+use function Pest\Laravel\assertDatabaseCount;
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertNotEquals;
+
+function assertSnapshotLoaded($snapshotName)
 {
-    /** @var \Spatie\DbSnapshots\Commands\Delete|m\Mock */
-    protected $command;
+    $nameOfTheLoadedSnapshot = getNameOfLoadedSnapshot();
 
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        $this->command = m::mock('Spatie\DbSnapshots\Commands\Load[choice]');
-
-        $this->app->bind('command.snapshot:load', function () {
-            return $this->command;
-        });
-    }
-
-    /** @test */
-    public function it_can_load_a_snapshot()
-    {
-        $this->assertSnapshotNotLoaded('snapshot2');
-
-        $this->command
-            ->shouldReceive('choice')
-            ->once()
-            ->andReturn('snapshot2');
-
-        Artisan::call('snapshot:load');
-
-        $this->assertSnapshotLoaded('snapshot2');
-    }
-
-    /** @test */
-    public function it_can_load_a_snapshot_via_streaming()
-    {
-        $this->assertSnapshotNotLoaded('snapshot2');
-
-        $this->command
-            ->shouldReceive('choice')
-            ->once()
-            ->andReturn('snapshot2');
-
-        Artisan::call('snapshot:load', [
-            '--stream' => true,
-        ]);
-
-        $this->assertSnapshotLoaded('snapshot2');
-    }
-
-    /** @test */
-    public function it_can_load_a_compressed_snapshot_via_streaming()
-    {
-        $this->assertSnapshotNotLoaded('snapshot4');
-
-        $this->command
-            ->shouldReceive('choice')
-            ->once()
-            ->andReturn('snapshot4');
-
-        Artisan::call('snapshot:load', [
-            '--stream' => true,
-        ]);
-
-        $this->assertSnapshotLoaded('snapshot4');
-    }
-
-    /** @test */
-    public function it_drops_tables_when_loading_a_snapshot()
-    {
-        DB::insert('insert into `users` (`id`, `name`) values (1, "test")');
-
-        $this->command
-            ->shouldReceive('choice')
-            ->once()
-            ->andReturn('snapshot2');
-
-        Artisan::call('snapshot:load');
-
-        $this->assertTableNotExists('users');
-    }
-
-    /** @test */
-    public function it_can_load_a_snapshot_without_dropping_existing_tables()
-    {
-        DB::insert('insert into `users` (`id`, `name`) values (1, "test")');
-
-        $this->command
-            ->shouldReceive('choice')
-            ->once()
-            ->andReturn('snapshot2');
-
-        Artisan::call('snapshot:load', ['--drop-tables' => 0]);
-
-        $this->assertDatabaseCount('users', 1);
-    }
-
-    /** @test */
-    public function it_can_load_a_snapshot_with_a_given_name()
-    {
-        $this->assertSnapshotNotLoaded('snapshot2');
-
-        Artisan::call('snapshot:load', ['name' => 'snapshot2']);
-
-        $this->assertSnapshotLoaded('snapshot2');
-    }
-
-    /** @test */
-    public function it_can_load_the_latest_snapshot()
-    {
-        $this->assertSnapshotNotLoaded('snapshot4');
-
-        Artisan::call('snapshot:load', ['--latest' => true]);
-
-        $this->assertSnapshotLoaded('snapshot4');
-    }
-
-    /** @test */
-    public function it_can_load_a_snapshot_with_connection_option()
-    {
-        $this->assertSnapshotNotLoaded('snapshot2');
-
-        Artisan::call('snapshot:load', ['name' => 'snapshot2', '--connection' => 'testing']);
-
-        $this->assertSnapshotLoaded('snapshot2');
-    }
-
-    /** @test */
-    public function it_can_load_a_compressed_snapshot()
-    {
-        $this->assertSnapshotNotLoaded('snapshot4');
-
-        Artisan::call('snapshot:load', ['name' => 'snapshot4']);
-
-        $this->assertSnapshotLoaded('snapshot4');
-    }
-
-    protected function assertSnapshotLoaded($snapshotName)
-    {
-        $this->assertEquals(
-            $snapshotName,
-            $this->getNameOfLoadedSnapshot(),
-            "Failed to assert that `{$snapshotName}` is loaded. Current snapshot: `{$this->getNameOfLoadedSnapshot()}`"
-        );
-    }
-
-    protected function assertSnapshotNotLoaded($snapshotName)
-    {
-        $this->assertNotEquals(
-            $snapshotName,
-            $this->getNameOfLoadedSnapshot(),
-            "Failed to assert that `{$snapshotName}` was not loaded."
-        );
-    }
-
-    protected function getNameOfLoadedSnapshot(): string
-    {
-        $result = DB::select('select `name` from models;');
-
-        return count($result) ? $result[0]->name : '';
-    }
+    assertEquals(
+        $snapshotName,
+        $nameOfTheLoadedSnapshot,
+        "Failed to assert that `{$snapshotName}` is loaded. Current snapshot: `{$nameOfTheLoadedSnapshot}`"
+    );
 }
+
+function assertSnapshotNotLoaded($snapshotName): void
+{
+    assertNotEquals(
+        $snapshotName,
+        getNameOfLoadedSnapshot(),
+        "Failed to assert that `{$snapshotName}` was not loaded."
+    );
+}
+
+function getNameOfLoadedSnapshot(): string
+{
+    $result = DB::select('select `name` from models;');
+
+    return count($result) ? $result[0]->name : '';
+}
+
+beforeEach(function () {
+    $this->command = m::mock('Spatie\DbSnapshots\Commands\Load[choice]');
+
+    $this->app->bind('command.snapshot:load', function () {
+        return $this->command;
+    });
+});
+
+it('can load a snapshot', function () {
+    assertSnapshotNotLoaded('snapshot2');
+
+    $this->command
+        ->shouldReceive('choice')
+        ->once()
+        ->andReturn('snapshot2');
+
+    Artisan::call('snapshot:load');
+
+    assertSnapshotLoaded('snapshot2');
+});
+
+it('can load a snapshot via streaming', function () {
+    assertSnapshotNotLoaded('snapshot2');
+
+    $this->command
+        ->shouldReceive('choice')
+        ->once()
+        ->andReturn('snapshot2');
+
+    Artisan::call('snapshot:load', [
+        '--stream' => true,
+    ]);
+
+    assertSnapshotLoaded('snapshot2');
+});
+
+it('can load a compressed snapshot via streaming', function () {
+    assertSnapshotNotLoaded('snapshot4');
+
+    $this->command
+        ->shouldReceive('choice')
+        ->once()
+        ->andReturn('snapshot4');
+
+    Artisan::call('snapshot:load', [
+        '--stream' => true,
+    ]);
+
+    assertSnapshotLoaded('snapshot4');
+});
+
+it('drops tables when loading a snapshot', function () {
+    DB::insert('insert into `users` (`id`, `name`) values (1, "test")');
+
+    $this->command
+        ->shouldReceive('choice')
+        ->once()
+        ->andReturn('snapshot2');
+
+    Artisan::call('snapshot:load');
+
+    assertTableNotExists('users');
+});
+
+it('can load a snapshot without dropping existing tables', function () {
+    DB::insert('insert into `users` (`id`, `name`) values (1, "test")');
+
+    $this->command
+        ->shouldReceive('choice')
+        ->once()
+        ->andReturn('snapshot2');
+
+    Artisan::call('snapshot:load', ['--drop-tables' => 0]);
+
+    assertDatabaseCount('users', 1);
+});
+
+it('can load a snapshot with a given name', function () {
+    assertSnapshotNotLoaded('snapshot2');
+
+    Artisan::call('snapshot:load', ['name' => 'snapshot2']);
+
+    assertSnapshotLoaded('snapshot2');
+});
+
+it('can load the latest snapshot', function () {
+    assertSnapshotNotLoaded('snapshot4');
+
+    Artisan::call('snapshot:load', ['--latest' => true]);
+
+    assertSnapshotLoaded('snapshot4');
+});
+
+it('can load a snapshot with connection option', function () {
+    assertSnapshotNotLoaded('snapshot2');
+
+    Artisan::call('snapshot:load', ['name' => 'snapshot2', '--connection' => 'testing']);
+
+    assertSnapshotLoaded('snapshot2');
+});
+
+it('can load a compressed snapshot', function () {
+    assertSnapshotNotLoaded('snapshot4');
+
+    Artisan::call('snapshot:load', ['name' => 'snapshot4']);
+
+    assertSnapshotLoaded('snapshot4');
+});
