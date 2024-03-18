b1bfc60af8a6368c4bc4d9216314eb1e9560495e
François M <francoism90@users.noreply.github.com>
2024-03-18 12:20:35 +0100

    Add mariadb

--- src/DbDumperFactory.php
+++ src/DbDumperFactory.php
@@ -55,7 +55,7 @@ class DbDumperFactory
     {
         $driver = strtolower($dbDriver);
 
-        if ($driver === 'mysql') {
+        if (in_array($driver, ['mariadb', 'mysql'])) {
             return new MySql();
         }
 
